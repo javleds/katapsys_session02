154472cddc854d7651e801bfd099f2e5a80b95b8
Javier Ledezma <jledezma@medtrainer.com>
2020-02-27 20:22:50 -0600

    From function to React class

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -2,14 +2,21 @@ import React from "react";
 import Avatar from "./Avatar";
 import UserName from "./UserName";
 
-const UserProfile = () => {
-	return (
-		<div className='user-profile'>
-			<Avatar />
-			<UserName />
-			<button>Ingresar</button>
-		</div>
-	);
-};
+class UserProfile extends React.Component {
+
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		return (
+			<div className='user-profile'>
+				<Avatar />
+				<UserName name='Javier Ledezma'/>
+				<button>Ingresar</button>
+			</div>
+		)
+	}
+}
 
 export default UserProfile;
