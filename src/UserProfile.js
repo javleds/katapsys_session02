660d7f13c9589b337fc81ea7030717c00ec27729
Javier Ledezma <jledezma@medtrainer.com>
2020-02-27 20:41:24 -0600

    Create conditional rendering

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -5,22 +5,30 @@ import UserName from "./UserName";
 class UserProfile extends React.Component {
 
 	constructor(props) {
-		super(props)
+		super(props);
 
 		this.state = {
 			isLoggedIn: true,
 			name: 'Javier Ledezma',
-		}
+		};
 	}
 
 	render() {
+		let button = null;
+
+		if (this.state.isLoggedIn) {
+			button = <button>Salir</button>;
+		} else {
+			button = <button>Ingresar</button>;
+		}
+
 		return (
 			<div className='user-profile'>
 				<Avatar />
 				<UserName name={this.state.name}/>
-				<button>Ingresar</button>
+				{button}
 			</div>
-		)
+		);
 	}
 }
 
