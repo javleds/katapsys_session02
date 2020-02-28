41b14937a6179917b1f9884ee496fde7a6afbaf8
Javier Ledezma <jledezma@medtrainer.com>
2020-02-27 20:21:03 -0600

    Add property

--- src/UserName.js
+++ src/UserName.js
@@ -1,7 +1,16 @@
 import React from "react";
+import propTypes from 'prop-types';
 
-const UserName = () => {
-	return <h1>Hola Oscar M</h1>;
+const UserName = (props) => {
+	return <h1>Hola {props.name}</h1>;
+};
+
+UserName.propTypes = {
+	name: propTypes.string.isRequired
+};
+
+UserName.defaultProps = {
+	name: 'Stranger'
 };
 
 export default UserName;
